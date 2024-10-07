78d5496de4bf0e33010e112a0418f3c07033f3e5
Srepin <px1099@gmail.com>
2024-10-07 17:10:10 +0800

    func var, variadic, deferred examples

--- examples/basic/func.go
+++ examples/basic/func.go
@@ -1,5 +1,7 @@
 package basic
 
+import "fmt"
+
 //lint:ignore U1000 (example)
 func sum_two_int(x, y int) int {
 	return x + y
@@ -40,4 +42,52 @@ func sum_int_slice(a []int) int {
 		sum += x
 	}
 	return sum
+}
+
+//lint:ignore U1000 (example)
+func sum_int(vals ...int) int {
+	// slice is a pointer => pass by reference
+	sum := 0
+	for x := range vals {
+		sum += x
+	}
+	return sum
+}
+
+//lint:ignore U1000 (example)
+func variadic_func_example() int {
+	vSlice := []int{1, 2, 3, 4}
+	return sum_int(vSlice...)
+}
+
+//lint:ignore U1000 (example)
+func deferred_example() {
+	// Args of deferred call are evaluated immediately
+	name := "Beta"
+	defer fmt.Println("Bye", name)  // Bye Beta
+	name = "Alpha"
+	fmt.Println("Hello", name)  // Hello Alpha
+}
+
+//lint:ignore U1000 (example)
+func func_var_example() func(int) int {
+	// return a function
+	var funcVar func(int) int
+	incFn := func(x int) int {
+		return x + 1
+	}
+	funcVar = incFn
+	return funcVar
+}
+
+//lint:ignore U1000 (example)
+func apply_func(fn func(int) int, x int) int {
+	// return a function
+	return fn(x)
+}
+
+//lint:ignore U1000 (example)
+func anon_func_example() {
+	// return a function
+	apply_func(func(x int) int {return x + 1}, 2)
 }
