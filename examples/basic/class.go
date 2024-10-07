91f9dcace2839d0d96c95f62d2ff559ce4472ae4
Srepin <px1099@gmail.com>
2024-10-07 18:07:10 +0800

    class example

---
+++ examples/basic/class.go
@@ -0,0 +1,34 @@
+package basic
+
+import "math"
+
+type MyInt int
+
+func (mi MyInt) Double() int {
+	// mi is passed by value (copied)
+	return int(mi * 2)
+}
+
+//lint:ignore U1000 (example)
+func class_example() {
+	v := MyInt(3)
+	_ = v.Double()
+}
+
+type Point struct {
+	// start with lowercase letter => public
+	x float64
+	y float64
+}
+
+// start with uppercase letter => public
+func (p Point) DistToOrig() float64 {
+	// p is passed by value (cannot change internal vals, copy when called)
+	return math.Sqrt(p.x*p.x + p.y*p.y)
+}
+
+func (p *Point) Scale(v float64) {
+	// p is passed by reference (pointer receivers)
+	p.x = p.x * v
+	p.y = p.y * v
+}
